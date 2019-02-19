e4e529b8df658615eefcb631eed24aff1b79524e
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2019-02-19 01:01:23 +0100

    wip create child page

--- src/Controller/Child/ChildCreateController.php
+++ src/Controller/Child/ChildCreateController.php
@@ -3,20 +3,23 @@
 namespace App\Controller\Child;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChildCreateController extends AbstractController
 {
     /**
-     * @Route("/child/{id}/create", name="child_create")
-     * @param string $id
+     * @Route("/child/create", name="child_create")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(string $id)
+    public function index(Request $request)
     {
-        return $this->render('child_create/index.html.twig', [
-            'controller_name' => 'ChildCreateController',
-        ]);
+
+        if($request->getMethod() === 'POST'){
+            dump($request->request->all());
+        }
+
+        return $this->render('child_create/index.html.twig', []);
     }
 }
