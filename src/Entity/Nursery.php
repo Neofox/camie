373417e8787f7a1855e5a28295df24ad5244186a
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2019-02-22 13:08:37 +0100

    add translation by nursery and add email and phone in db

--- src/Entity/Nursery.php
+++ src/Entity/Nursery.php
@@ -33,6 +33,16 @@ class Nursery
      */
     private $children;
 
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
     public function __construct()
     {
         $this->users = new ArrayCollection();
@@ -42,6 +52,11 @@ class Nursery
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getSlugName(): string
+    {
+        return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $this->getName())));
     }
 
     public function getName(): ?string
@@ -114,6 +129,30 @@ class Nursery
                 $child->setNursery(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
 
         return $this;
     }
