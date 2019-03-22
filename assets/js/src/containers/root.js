4d657a7a40530ee5783307557dd985f3de659fd3
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2019-03-23 00:33:08 +0100

    add basic transition

--- assets/js/src/containers/root.js
+++ assets/js/src/containers/root.js
@@ -1,16 +1,31 @@
-import { Route } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import React from "react";
 import ChildrenList from "../containers/childrenList";
 import Child from "../containers/child";
 import SheetsList from "../containers/sheetsList";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const Root = () => {
     return (
-        <div>
-            <Route path={"/"} exact component={ChildrenList} />
-            <Route path={"/child/:id/history"} exact component={SheetsList} />
-            <Route path={"/child/:id"} exact component={Child} />
-        </div>
+        <Route
+            render={({ location }) => {
+                return (
+                    <TransitionGroup component={null}>
+                        <CSSTransition
+                            timeout={300}
+                            classNames="fade"
+                            key={location.key}
+                        >
+                            <Switch location={location}>
+                                <Route path={"/"} exact component={ChildrenList} />
+                                <Route path={"/child/:id/history"} exact component={SheetsList} />
+                                <Route path={"/child/:id"} exact component={Child} />
+                            </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
+                );
+            }}
+        />
     );
 };
 
